d244029619b88d25c4ec9e4c05e799977fe36f79
Dominik Mikiewicz <dominik@cartomatic.pl>
2015-12-15 15:35:58 +0100

    Add json seralisation helpers

---
+++ Serialization.Tests/JsonSerializationExtensionsTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Cartomatic.Utils.Serialisation;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Cartomatic.Serialization.Tests
+{
+    [TestFixture]
+    public class JsonSerializationExtensionsTests
+    {
+        [Test]
+        public void FromJson_WhenPassedArbitraryObject_DeserializesItProperly()
+        {
+            string json = MakeTestJson();
+            var refObj = MakeTestObject();
+
+            var obj = json.DeserializeFromJson<TestObjectWithSimpleTypes>();
+
+            obj.ShouldBeEquivalentTo(refObj);
+        }
+
+        [Test]
+        public void DeserializeFromJson_WhenPassedInvalidJson_ShouldNotThrowButReturnNullByDefault()
+        {
+            var invalidJson = MakeInvalidTestJson();
+            var obj = invalidJson.DeserializeFromJson<TestObjectWithSimpleTypes>();
+
+            obj.Should().BeNull();
+        }
+
+        [Test]
+        public void DeserializeFromJson_WhenPassedInvalidJsonAndSilentSetToTrue_ShouldThrow()
+        {
+            var json = MakeInvalidTestJson();
+            Action act = () => json.DeserializeFromJson<TestObjectWithSimpleTypes>(false);
+
+            act.ShouldThrow<Exception>();
+        }
+
+        [Test]
+        public void ToJson_WhenCalledOnArbitraryObject_SerializesItProperly()
+        {
+            var obj = MakeTestObject();
+            var refJson = MakeTestJson();
+
+            string json = obj.SerializeToJson();
+
+            json.Should().BeSameAs(refJson);
+        }
+
+        private TestObjectWithSimpleTypes MakeTestObject()
+        {
+            return new TestObjectWithSimpleTypes()
+            {
+                PropBool = true,
+                PropEnum = TestEnum.Value2,
+                PropInt = 666,
+                PropDbl = 1.234,
+                PropString = "someText"
+            };
+        }
+
+        private string MakeTestJson()
+        {
+            return "{PropBool: true, PropEnum: 1, PropInt: 666, PropDbl: 1.234, PropString: 'someText'}";
+        }
+
+        private string MakeInvalidTestJson()
+        {
+            return "{PropBool: 'x', PropEnum: 1, PropInt: 666, PropDbl: 1.234, PropString: 'someText'}";
+        }
+
+
+        [DataContract]
+        private class TestObjectWithSimpleTypes
+        {
+            [DataMember]
+            public bool PropBool { get; set; }
+
+            [DataMember]
+            public TestEnum PropEnum { get; set; }
+
+            [DataMember]
+            public int PropInt { get; set; }
+
+            [DataMember]
+            public double PropDbl { get; set; }
+
+            [DataMember]
+            public string PropString { get; set; }
+
+        }
+
+
+        public enum TestEnum
+        {
+            Value1,
+            Value2
+        }
+    }
+}
---
+++ Serialization/JsonSerializationExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Cartomatic.Utils.Serialisation
+{
+    public static class JsonSerializationExtensions
+    {
+        /// <summary>
+        /// Deserializes json string to the type specified
+        /// </summary>
+        /// <typeparam name="T">Type to deserialize to</typeparam>
+        /// <param name="json"></param>
+        /// <param name="silent"></param>
+        /// <returns></returns>
+        public static T DeserializeFromJson<T>(this string json, bool silent = true)
+        {
+            return (T) json.DeserializeFromJson(typeof (T), silent);
+        }
+
+        public static object DeserializeFromJson(this string json, Type destinationType, bool silent = true)
+        {
+            object output = null;
+
+            if (!string.IsNullOrEmpty(json))
+            {
+                try
+                {
+                    output = JsonConvert.DeserializeObject(json, destinationType);
+                }
+                catch (Exception ex)
+                {
+                    if (!silent)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return output;
+        }
+
+
+        /// <summary>
+        /// Serializes object to json string
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static string SerializeToJson(this object o)
+        {
+            return JsonConvert.SerializeObject(o);
+        }
+    }
+}
