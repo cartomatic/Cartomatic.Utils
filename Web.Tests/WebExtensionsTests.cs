abec7c24b544e7980dbae10e71837c88cff7174a
Dominik Mikiewicz <dominik@cartomatic.pl>
2015-12-16 15:57:22 +0100

    Add web request utils

---
+++ Web.Tests/WebExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Cartomatic.Utils.Web.Tests
+{
+    [TestFixture]
+    public class WebExtnsionTests
+    {
+        [Test]
+        public void CreateHttpWebRequest_WhenPassingUrlString_CreatesWebRequestWithProperUrl()
+        {
+            var url = "http://subdomain.somesite.net:1234/p1/p2/?x=y";
+            var wr = url.CreateHttpWebRequest();
+
+            wr.RequestUri.AbsoluteUri.Should().Be(url);
+        }
+    }
+}
---
+++ Web/WebRequestExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartomatic.Utils.Web
+{
+    public static class WebRequestExtensions
+    {
+        public static HttpWebRequest CreateHttpWebRequest(this string url)
+        {
+            HttpWebRequest output = null;
+
+            try
+            {
+                output = (HttpWebRequest)HttpWebRequest.Create(url);
+            }
+            catch { }
+
+            return output;
+        }
+    }
+}
