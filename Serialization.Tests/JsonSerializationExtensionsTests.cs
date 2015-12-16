04af579e008db086479277a449bcc0f82f39b85a
Dominik Mikiewicz <dominik@cartomatic.pl>
2015-12-16 15:54:25 +0100

    serialization - remove some unused import

--- Serialization.Tests/JsonSerializationExtensionsTests.cs
+++ Serialization.Tests/JsonSerializationExtensionsTests.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.Serialization;
-using Cartomatic.Utils.Serialization;
 using FluentAssertions;
 using NUnit.Framework;
 
