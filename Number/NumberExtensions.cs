a155d0ae274bb59041875313dfd8ca932ec4d89b
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-01-11 11:20:48 +0100

    Some basic number utils

---
+++ Number/NumberExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartomatic.Utils.Number
+{
+    public static class NumberExtensions
+    {
+        /// <summary>
+        /// Fits a number in range if it is outside (to the closest bound)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static double FitInRange(this double n, double min, double max)
+        {
+            if (n > max) n = max;
+            if (n < min) n = min;
+
+            return n;
+        }
+
+        public static float FitInRange(this float n, double min, double max)
+        {
+            return (float) ((double) n).FitInRange(min, max);
+        }
+    }
+}
