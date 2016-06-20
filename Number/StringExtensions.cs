5c7e82e17f520d86d30fff1d1d1030192adf38b0
Dominik Mikiewicz <dominik@cartomatic.pl>
2016-06-20 17:15:34 +0200

    Add some IsNumeric string / object extensions

---
+++ Number/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartomatic.Utils.Number
+{
+    public static class StringExtensions
+    {
+        public static bool IsNumeric(this string wouldBeANumber)
+        {
+            double num;
+
+            return double.TryParse(wouldBeANumber, System.Globalization.NumberStyles.Any, NumberFormatInfo.InvariantInfo, out num);
+        }
+
+        public static bool IsNumeric(this object wouldBeANumber)
+        {
+            return Convert.ToString(wouldBeANumber, CultureInfo.InvariantCulture).IsNumeric();
+        }
+    }
+}
