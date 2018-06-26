afc9c407246714b1c859b48ef3c75b7c95b3aed8
Dominik Mikiewicz <dominik@cartomatic.pl>
2018-06-26 23:22:31 +0200

    Port crypto utils

---
+++ Utils/Crypto/Generator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartomatic.Utils.Crypto
+{
+    /// <summary>
+    /// some crypto related generator helpers.
+    /// </summary>
+    public class Generator
+    {
+        /// <summary>
+        /// Generates random string
+        /// </summary>
+        /// <param name="length">Length of byte array to be used</param>
+        /// <param name="useBase64Encoding">Whether or not use base64 encoding for the output</param>
+        /// <returns></returns>
+        public static string GenerateRandomString(int length, bool useBase64Encoding)
+        {
+            //Create a random key using a random number generator
+            var saltB = new byte[length];
+
+            //RNGCryptoServiceProvider is an implementation of a random number generator. 
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                //The array is now filled with cryptographically strong random bytes.
+                rng.GetBytes(saltB);
+            }
+
+            return useBase64Encoding ? Convert.ToBase64String(saltB) : BitConverter.ToString(saltB).Replace("-", "").ToLower();
+        }
+
+        /// <summary>
+        /// Generates a random string of declared length
+        /// </summary>
+        /// <param name="length">Length of string to be returned</param>
+        /// <returns>Random string</returns>
+        public static string GenerateRandomString(int length)
+        {
+            return GenerateRandomString(length, false).Substring(0, length);
+        }
+    }
+}
---
+++ Utils/Crypto/Hasher.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartomatic.Utils.Crypto
+{
+    public class Hasher
+    {
+        /// <summary>
+        /// Hashes a string using one of the sha algorithms - 1, 256 or 512
+        /// </summary>
+        /// <param name="s">Input string</param>
+        /// <param name="shaVersion">Version of the sha algorythm - Sha1, Sha256, Sha512; defaults to Sha1</param>
+        /// <param name="useBase64Encoding">Whether or not use base64 encoding for a returned string</param>
+        /// <returns>Hashed string</returns>
+        public static string ComputeShaHash(string s, int shaVersion, bool useBase64Encoding = false)
+        {
+            //get an array of bytes off the string
+            byte[] sb = System.Text.Encoding.UTF8.GetBytes(s);
+
+            //container for hashed string
+            byte[] ob;
+
+            switch (shaVersion)
+            {
+                case 1:
+                default:
+                    using (SHA1CryptoServiceProvider sha = new SHA1CryptoServiceProvider())
+                    {
+                        ob = sha.ComputeHash(sb);
+                    }
+                    break;
+
+                case 256:
+                    using (SHA256CryptoServiceProvider sha = new SHA256CryptoServiceProvider())
+                    {
+                        ob = sha.ComputeHash(sb);
+                    }
+                    break;
+
+                case 512:
+                    using (SHA512CryptoServiceProvider sha = new SHA512CryptoServiceProvider())
+                    {
+                        ob = sha.ComputeHash(sb);
+                    }
+                    break;
+            }
+
+            if (useBase64Encoding)
+            {
+                return Convert.ToBase64String(ob);
+            }
+            else
+            {
+                //remove the dashes and make string lowercase
+                return BitConverter.ToString(ob).Replace("-", "").ToLower();
+            }
+        }
+
+        /// <summary>
+        /// computes an md5 hash
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="useBase64Encoding"></param>
+        /// <returns></returns>
+        public static string ComputeMd5Hash(string s, bool useBase64Encoding = false)
+        {
+            //get an array of bytes off the string
+            byte[] sb = System.Text.Encoding.UTF8.GetBytes(s);
+
+            //container for hashed string
+            byte[] ob;
+
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                ob = md5.ComputeHash(sb);
+            }
+
+            if (useBase64Encoding)
+            {
+                return Convert.ToBase64String(ob);
+            }
+            else
+            {
+                //remove the dashes and make string lowercase
+                return BitConverter.ToString(ob).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
