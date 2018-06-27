aba5c8d26040c88823000a44a4500b09214f5c12
Dominik Mikiewicz <dominik@cartomatic.pl>
2018-06-27 08:26:57 +0200

    ported email utils

---
+++ Cartomatic.Utils/Email/IEmailTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Cartomatic.Utils.Email
+{
+    public interface IEmailTemplate
+    {
+        /// <summary>
+        /// Email template as fed to email sender
+        /// </summary>
+        string Title { get; set; }
+
+        string Body { get; set; }
+        bool IsBodyHtml { get; set; }
+
+        /// <summary>
+        /// Prepares the template based on a collection of token/value to be applied
+        /// </summary>
+        /// <param name="tokens"></param>
+        /// <returns></returns>
+        IEmailTemplate Prepare(IDictionary<string, object> tokens);
+    }
+}
