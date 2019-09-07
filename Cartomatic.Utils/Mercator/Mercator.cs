135a52e68b5f93e4ef2b60981dd6c9989a2b4a17
Dominik Mikiewicz <dominik@cartomatic.pl>
2019-09-07 11:11:45 +0200

    add some more mercator utils

--- Cartomatic.Utils/Mercator/Mercator.cs
+++ Cartomatic.Utils/Mercator/Mercator.cs
@@ -8,10 +8,17 @@ namespace Cartomatic.Utils
     {
         //borrowed from https://gist.github.com/onderaltintas/6649521
 
+        private const double EarthRadius = 6378137;
+
+
         /// <summary>
         /// 180 equatorial degrees in metres
         /// </summary>
-        private const double SphericalMercatorHalphSphere = 20037508.34;
+        private const double SphericalMercatorHalfSphere = EarthRadius * Math.PI;
+
+        private const double SphericalMercatorSphere = SphericalMercatorHalfSphere * 2;
+
+
 
         /// <summary>
         /// Converts lon lat to spherical mercator
@@ -22,10 +29,10 @@ namespace Cartomatic.Utils
         public static (double x, double y) FromLonLat(double lon, double lat)
         {
             //20037508.34 is spherical mercator  half sphere (0 - 180)
-            var x = lon * SphericalMercatorHalphSphere / 180;
+            var x = lon * SphericalMercatorHalfSphere / 180;
 
             var y = Math.Log(Math.Tan((90 + lat) * Math.PI / 360)) / (Math.PI / 180);
-            y *= 20037508.34 / 180;
+            y *= SphericalMercatorHalfSphere / 180;
 
             return (x, y);
         }
@@ -38,10 +45,33 @@ namespace Cartomatic.Utils
         /// <returns></returns>
         public static (double lon, double lat) ToLonLat(double x, double y)
         {
-            var lon = x * 180 / SphericalMercatorHalphSphere;
-            var lat = Math.Atan(Math.Exp(y * Math.PI / SphericalMercatorHalphSphere)) * 360 / Math.PI - 90;
+            var lon = x * 180 / SphericalMercatorHalfSphere;
+            var lat = Math.Atan(Math.Exp(y * Math.PI / SphericalMercatorHalfSphere)) * 360 / Math.PI - 90;
 
             return (lon, lat);
+        }
+
+
+        /// <summary>
+        /// Gets a spherical mercator tile bounding box from its grid address
+        /// </summary>
+        /// <param name="z"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="buffer">Buffer to add to each side of a bounding box in order to extend it. buffer is expressed in pixels at a given zoom level</param>
+        /// <returns></returns>
+        public static (double l, double b, double r, double t) GetSphericalMercatorBboxFromTileAddress(int z, int x, int y, int buffer = 0)
+        {
+            var tileSizeAtZoom = SphericalMercatorSphere / Math.Pow(2, z);
+            var pixelSizeAtZoom = tileSizeAtZoom / 256;
+            var collar = buffer * pixelSizeAtZoom;
+
+            return (
+                -SphericalMercatorHalfSphere + x * tileSizeAtZoom - collar,
+                SphericalMercatorHalfSphere - y * tileSizeAtZoom + collar,
+                -SphericalMercatorHalfSphere + x * tileSizeAtZoom + tileSizeAtZoom + collar,
+                SphericalMercatorHalfSphere - y * tileSizeAtZoom - tileSizeAtZoom - collar
+            );
         }
     }
 }
