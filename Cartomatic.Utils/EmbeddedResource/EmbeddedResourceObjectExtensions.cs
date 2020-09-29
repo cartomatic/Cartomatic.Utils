4702bdf548cfb485014db6a5b6702f78e8fe5f88
Dominik Mikiewicz <dominik@cartomatic.pl>
2020-09-29 17:19:54 +0200

    add some stra apis to embedded resource

--- Cartomatic.Utils/EmbeddedResource/EmbeddedResourceObjectExtensions.cs
+++ Cartomatic.Utils/EmbeddedResource/EmbeddedResourceObjectExtensions.cs
@@ -11,35 +11,47 @@ namespace Cartomatic.Utils.EmbeddedResource
         /// <summary>
         /// Gets a stream for an embedded resource
         /// </summary>
-        /// <remarks>
-        /// ported from maphive
-        /// </remarks>
         /// <param name="obj"></param>
         /// <param name="fullyQualifiedResourceName"></param>
         /// <returns></returns>
         public static Stream GetEmbeddedResourceStream(this object obj, string fullyQualifiedResourceName)
         {
-            var assembly = obj.GetType().Assembly;
+            return obj.GetType().GetEmbeddedResourceStream(fullyQualifiedResourceName);
+        }
+
+        /// <summary>
+        /// Gets a stream for an embedded resource
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="fullyQualifiedResourceName"></param>
+        /// <returns></returns>
+        public static Stream GetEmbeddedResourceStream(this Type type, string fullyQualifiedResourceName)
+        {
+            var assembly = type.Assembly;
             return assembly.GetManifestResourceStream(fullyQualifiedResourceName);
         }
 
         /// <summary>
         /// Gets embedded resource of an object's assembly
         /// </summary>
-        /// <remarks>
-        /// ported from maphive
-        /// </remarks>
         /// <param name="obj"></param>
         /// <param name="fullyQualifiedResourceName"></param>
         /// <returns></returns>
         public static byte[] GetEmbeddedResource(this object obj, string fullyQualifiedResourceName)
         {
-            byte[] resourceData;
+            return obj.GetType().GetEmbeddedResource(fullyQualifiedResourceName);
+        }
 
-            using (var stream = obj.GetEmbeddedResourceStream(fullyQualifiedResourceName))
-            {
-                resourceData = stream.ReadStream();
-            }
+        /// <summary>
+        /// Gets embedded resource of an object's assembly
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="fullyQualifiedResourceName"></param>
+        /// <returns></returns>
+        public static byte[] GetEmbeddedResource(this Type type, string fullyQualifiedResourceName)
+        {
+            using var stream = type.GetEmbeddedResourceStream(fullyQualifiedResourceName);
+            var resourceData = stream.ReadStream();
 
             return resourceData;
         }
@@ -47,21 +59,25 @@ namespace Cartomatic.Utils.EmbeddedResource
         /// <summary>
         /// Gets embedded resource string content
         /// </summary>
-        /// <remarks>
-        /// ported from maphive
-        /// </remarks>
         /// <param name="obj"></param>
         /// <param name="fullyQualifiedResourceName"></param>
         /// <returns></returns>
         public static string GetEmbeddedResourceStringContent(this object obj, string fullyQualifiedResourceName)
         {
-            var content = string.Empty;
+            return obj.GetType().GetEmbeddedResourceStringContent(fullyQualifiedResourceName);
+        }
 
-            using(var str = obj.GetEmbeddedResourceStream(fullyQualifiedResourceName))
-            using (var rdr = new StreamReader(str))
-            {
-                content = rdr.ReadToEnd();
-            }
+        /// <summary>
+        /// Gets embedded resource string content
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="fullyQualifiedResourceName"></param>
+        /// <returns></returns>
+        public static string GetEmbeddedResourceStringContent(this Type type, string fullyQualifiedResourceName)
+        {
+            using var str = type.GetEmbeddedResourceStream(fullyQualifiedResourceName);
+            using var rdr = new StreamReader(str);
+            var content = rdr.ReadToEnd();
 
             return content;
         }
