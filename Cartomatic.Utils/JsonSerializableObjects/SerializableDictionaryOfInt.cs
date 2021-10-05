ab8e646237d777c2454c1926e8d23c307dc0963b
Dominik Mikiewicz <dominik@cartomatic.pl>
2021-10-05 21:52:13 +0200

    add srializable dict of int

---
+++ Cartomatic.Utils/JsonSerializableObjects/SerializableDictionaryOfInt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartomatic.Utils.JsonSerializableObjects
+{
+    /// <summary>
+    /// explicit implementation of a serializable dictionary for int
+    /// </summary>
+    public class SerializableDictionaryOfInt : SerializableDictionary<int>
+    {
+    }
+}
