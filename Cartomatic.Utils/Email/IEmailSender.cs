97345ee37a725fd98506264a9bc018b060243553
Dominik Mikiewicz <dominik@cartomatic.pl>
2022-02-12 14:15:30 +0100

    minor tweaks in email sender

--- Cartomatic.Utils/Email/IEmailSender.cs
+++ Cartomatic.Utils/Email/IEmailSender.cs
@@ -1,4 +1,7 @@
-﻿namespace Cartomatic.Utils.Email
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Cartomatic.Utils.Email
 {
     /// <summary>
     /// Email sender
@@ -12,5 +15,31 @@
         /// <param name="emailTemplate"></param>
         /// <param name="recipient"></param>
         void Send(IEmailAccount emailAccount, IEmailTemplate emailTemplate, string recipient);
+
+        /// <summary>
+        /// Sends an email
+        /// </summary>
+        /// <param name="emailAccount"></param>
+        /// <param name="emailTemplate"></param>
+        /// <param name="recipient"></param>
+        /// <param name="logger"></param>
+        void Send(IEmailAccount emailAccount, IEmailTemplate emailTemplate, string recipient, Action<string> logger);
+
+        /// <summary>
+        /// Sends an email
+        /// </summary>
+        /// <param name="emailAccount"></param>
+        /// <param name="emailTemplate"></param>
+        /// <param name="recipient"></param>
+        Task SendAsync(IEmailAccount emailAccount, IEmailTemplate emailTemplate, string recipient);
+
+        /// <summary>
+        /// Sends an email
+        /// </summary>
+        /// <param name="emailAccount"></param>
+        /// <param name="emailTemplate"></param>
+        /// <param name="recipient"></param>
+        /// <param name="logger"></param>
+        Task SendAsync(IEmailAccount emailAccount, IEmailTemplate emailTemplate, string recipient, Action<string> logger);
     }
 }
